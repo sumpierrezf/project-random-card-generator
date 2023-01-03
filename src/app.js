2fa64d65be8adab97af88d1140a7720b40b8173c
sumpierrezf <viqui09umpierrez@gmail.com>
2023-01-03 20:13:52 +0000

    proyecto generando cartas al azar

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var palos = ["♦", "♥", "♠", "♣"];
+
+  const numeros = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "Q",
+    "J",
+    "As"
+  ];
+  let indexPalos = Math.floor(Math.random() * palos.length);
+  let indexNumeros = Math.floor(Math.random() * numeros.length);
+
+  let color =
+    palos[indexPalos] == "♦" || palos[indexPalos] == "♥" ? "red" : "black";
+
+  console.log(palos[indexPalos]);
+  let top = document.querySelector(".top");
+  let bottom = document.querySelector(".bottom");
+  let number = document.querySelector(".number");
+  top.innerHTML = palos[indexPalos];
+  document.querySelector(".top").style.color = color;
+  document.querySelector(".bottom").style.color = color;
+  document.querySelector(".number").style.color = color;
+  bottom.innerHTML = palos[indexPalos];
+  number.innerHTML = numeros[indexNumeros];
 };
